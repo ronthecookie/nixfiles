b3b90eeea2a3df90be7b270c45b9fdc7c579adf1
ckie <me@ronthecookie.me>
2021-07-09 21:14:11 +0300

    mailserver: add `make emails` to get processed emails

--- modules/services/mailserver/util.nix
+++ modules/services/mailserver/util.nix
@@ -12,7 +12,8 @@ with builtins; {
   */
   process = salt: aliases:
     (map (x:
-      (x + "-" + (substring 0 7
-        (hashString "sha512" "${x}${salt}"))
+      (x + "-" + (substring 0 7 (hashString "sha512" "${x}${salt}"))
         + "@ckie.dev")) aliases);
+
+  default-aliases = [ "nixpkgs" "github" ];
 }
